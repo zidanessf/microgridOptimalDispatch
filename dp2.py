b320309f2362af6f456b7c779a0172f25d3f08a9
Master Shi <zidane765@hotmail.com>
2017-09-21 21:05:26 +0800

    Initial commit

---
+++ dp2.py
@@ -0,0 +1,226 @@
+from importFile import *
+from microgrid import *
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+optimalDispatch = ConcreteModel(name = 'IES_optimalDispatch')
+'''Initialize a special case of microgrid'''
+microgrid_device = dict()
+microgrid_device['PV_1'] = PV()
+microgrid_device['ES_1'] = electricStorage()
+microgrid_device['ABSC_1'] = absorptionChiller()
+microgrid_device['Boiler_1'] = boiler()
+microgrid_device['CS_1'] = coldStorage()
+microgrid_device['AC_1'] = airConditioner()
+microgrid_device['GT_1'] = gasTurbine()
+microgrid_device['ut'] = utility()
+microgrid_device['inv'] = inverter()
+'''Load input data'''
+microgrid_data = pd.read_excel('input.xlsx')
+electricalLoad = (microgrid_data['交流负荷'] + microgrid_data['直流负荷']).tolist()
+pv_output = microgrid_data['光伏出力'].tolist()
+microgrid_device['ut'].buy_price = microgrid_data['电价'].tolist()
+cold_load = microgrid_data['冷负荷'].tolist()
+water_heat_load = microgrid_data['热水负荷'].tolist()
+steam_heat_load = microgrid_data['蒸汽负荷'].tolist()
+'''A general model and algorithm for microgrid optimal dispatch'''
+'''define sets'''
+N_T = 95
+T = RangeSet(0,N_T)
+step = 24/N_T
+N_es = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],electricStorage)]
+N_absc = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],absorptionChiller)]
+N_bol = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],boiler)]
+N_cs = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],coldStorage)]
+N_ac = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],airConditioner)]
+N_gt = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],gasTurbine)]
+N_pv = [key for key in microgrid_device.keys() if isinstance(microgrid_device[key],PV)]
+N_dev = [key for key in microgrid_device.keys()]
+'''define variables'''
+#electrical storage
+optimalDispatch.es_power_in = Var(N_es,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Pmax_in))
+optimalDispatch.es_power_out =Var(N_es,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Pmax_out))
+optimalDispatch.es_energy = Var(N_es,T,bounds=lambda mdl,i,T:(microgrid_device[i].SOCmin*microgrid_device[i].capacity,microgrid_device[i].SOCmax*microgrid_device[i].capacity))
+#absorption chiller
+optimalDispatch.absc_heat_in = Var(N_absc,T,domain=PositiveReals)
+#heat variables
+optimalDispatch.high_heat = Var(T)
+optimalDispatch.medium_heat = Var(T)
+optimalDispatch.low_heat = Var(T)
+#boiler
+optimalDispatch.bol_power = Var(N_bol,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Pmax))
+#cold storage
+optimalDispatch.cs_power = Var(N_cs,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Pmax))
+optimalDispatch.cs_cold_in = Var(N_cs,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Hin))
+optimalDispatch.cs_cold_out = Var(N_cs,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Hout))
+optimalDispatch.cs_cold_stored = Var(N_cs,T,bounds=lambda mdl,i,T:(microgrid_device[i].Tmin*microgrid_device[i].capacity,microgrid_device[i].Tmax*microgrid_device[i].capacity))
+#air conditioner
+optimalDispatch.ac_power = Var(N_ac,T,bounds=lambda mdl,i,T:(0,microgrid_device[i].Pmax))
+#gas turbine
+optimalDispatch.gt_power = Var(N_gt,T,bounds=lambda mdl,i,T:(microgrid_device[i].Pmin,microgrid_device[i].Pmax))
+#inverter
+optimalDispatch.inv_ac = Var(T) #inv_ac > 0 means energy flows from inverter to ac side
+optimalDispatch.inv_dc = Var(T) #inv_dc > 0 means energy flows from inverter to dc side
+#utility power
+optimalDispatch.utility_power = Var(T,domain=PositiveReals)
+#TODO add ac-dc inverter variables
+'''define disjuncts(states)'''
+'''Battery'''
+def es_in_out_state(b,i,T,indicator):
+    mdl = b.model()
+    if indicator == 0:
+        b.es_forbidden = Constraint(expr=mdl.es_power_in[i,T]==10)
+    else:
+        b.es_forbidden = Constraint(expr=mdl.es_power_out[i, T] == 10)
+optimalDispatch.es_power_in_out_state = Disjunct(N_es,T,[0,1],rule=es_in_out_state)
+def es_in_out(mdl,i,T):
+    return [mdl.es_power_in_out_state[i,T,0],mdl.es_power_in_out_state[i,T,1]]
+optimalDispatch.es_in_out = Disjunction(N_es,T,rule=es_in_out)
+'''Cold Storage'''
+def cs_cold_in_out_state(b,i,T,indicator):
+    mdl = b.model()
+    if indicator == 0:
+        b.cs_forbidden = Constraint(expr=mdl.cs_cold_in[i, T] == 0)
+    else:
+        b.cs_forbidden = Constraint(expr=mdl.cs_cold_out[i, T] == 0)
+optimalDispatch.cs_cold_in_out_state = Disjunct(N_cs,T,[0,1],rule=cs_cold_in_out_state)
+def cs_in_out(mdl,i,T):
+    return [mdl.cs_cold_in_out_state[i,T,0],mdl.cs_cold_in_out_state[i,T,1]]
+optimalDispatch.cs_in_out = Disjunction(N_cs,T,rule=cs_in_out)
+'''define constraints'''
+'''电功率平衡约束'''
+def electricalPowerBalance(mdl,t):
+    power_supply = sum(mdl.es_power_out[i,t] for i in N_es) \
+                   + sum(mdl.gt_power[i,t] for i in N_gt) \
+                   + mdl.utility_power[t]
+    power_demand = sum(mdl.es_power_in[i,t] for i in N_es) \
+                   + sum(mdl.cs_power[i,t] for i in N_cs) \
+                   + sum(mdl.ac_power[i,t] for i in N_ac) \
+                   + electricalLoad[t]
+    return power_supply == power_demand
+optimalDispatch.electricalPowerBalance = Constraint(T,rule=electricalPowerBalance)
+'''热功率平衡约束'''
+H2M = 0.3
+M2L = 0.3
+def heatPowerBalance(mdl,t):
+    heat_supply = sum(mdl.bol_power[i,t] for i in N_bol) \
+                  + sum(microgrid_device[i].HER*microgrid_device[i].heat_recycle*mdl.gt_power[i,t] for i in N_gt)
+    heat_demand = sum(mdl.absc_heat_in[i,t] for i in N_absc) \
+                  + water_heat_load[t]
+    return heat_supply >= heat_demand
+optimalDispatch.heatPowerBalance = Constraint(T,rule=heatPowerBalance)
+optimalDispatch.HPB1 = Constraint(T,rule=lambda mdl,t:mdl.high_heat[t]+sum(mdl.bol_power[n_bol,t] for n_bol in N_bol)>=steam_heat_load[t])
+optimalDispatch.HPB2 = Constraint(T,rule=lambda mdl,t:mdl.medium_heat[t]==H2M*steam_heat_load[t]+sum(mdl.gt_power[n_gt,t]*microgrid_device[n_gt].HER*microgrid_device[n_gt].heat_recycle for n_gt in N_gt))
+optimalDispatch.HPB3 = Constraint(T,rule=lambda mdl,t:mdl.medium_heat[t] >= sum(mdl.absc_heat_in[n_absc,t] for n_absc in N_absc))
+optimalDispatch.HPB4 = Constraint(T,rule=lambda mdl,t:mdl.low_heat[t] == (M2L-1) * sum(mdl.absc_heat_in[n_absc,t] for n_absc in N_absc) + mdl.medium_heat[t])
+optimalDispatch.HPB5 = Constraint(T,rule=lambda mdl,t:mdl.low_heat[t] >= water_heat_load[t])
+#TODO fix bugs
+'''冷功率平衡约束'''
+def coldPowerBalance(mdl,t):
+    cold_supply = sum(mdl.ac_power[i,t]*microgrid_device[i].EER for i in N_ac) \
+                  +sum((mdl.cs_power[i,t]*microgrid_device[i].EER-mdl.cs_cold_in[i,t]) for i in N_cs)\
+                  +sum(mdl.cs_cold_out[i,t] for i in N_cs)\
+                  +sum(mdl.absc_heat_in[i,t] * microgrid_device[i].COP_htc for i in N_absc)
+    cold_demand = cold_load[t]
+    return cold_supply == cold_demand
+optimalDispatch.coldPowerBalance = Constraint(T,rule=coldPowerBalance)
+optimalDispatch.ChillerMoreThanColdIn = Constraint(T,N_cs,rule=lambda mdl,t,n_cs:mdl.cs_power[n_cs,t]*microgrid_device[n_cs].EER>=mdl.cs_cold_in[n_cs,t])
+'''电池日平衡约束、自放电率、爬坡率约束'''
+def batteryEnergyBalance(mdl,n_es,t):
+    bat = microgrid_device[n_es]
+    if t == 95:
+        return mdl.es_energy[n_es,t] == mdl.es_energy[n_es,0]
+    elif t == 0:
+        return mdl.es_energy[n_es,t] == bat.SOCint * bat.capacity
+    else:
+        return mdl.es_energy[n_es,t] == mdl.es_energy[n_es,t - 1] * (1-bat.selfRelease) \
+                                        + step * (bat.efficiency*mdl.es_power_in[n_es,t-1] - (1/bat.efficiency)*mdl.es_power_out[n_es,t-1])
+optimalDispatch.batteryEnergyBalance = Constraint(N_es,T,rule=batteryEnergyBalance)
+def batteryRampLimit(mdl,n_es,t):
+    if t == 0:
+        return Constraint.Skip
+    else:
+        return -microgrid_device[n_es].maxDetP <= (mdl.es_power_out[n_es,t] - mdl.es_power_in[n_es,t]) - (mdl.es_power_out[n_es,t-1] - mdl.es_power_in[n_es,t-1]) <= microgrid_device[n_es].maxDetP
+optimalDispatch.batteryRampLimit = Constraint(N_es,T,rule=batteryRampLimit)
+'''冰蓄冷日平衡约束、自放冷率、爬坡率约束'''
+def coldStorageEnergyBalance(mdl,n_cs,t):
+    ice = microgrid_device[n_cs]
+    if t == 0:
+        return mdl.cs_cold_stored[n_cs,t] == ice.Tint * ice.capacity
+    elif t == 95:
+        return mdl.cs_cold_stored[n_cs,t] == mdl.cs_cold_stored[n_cs,0]
+    else:
+        return mdl.cs_cold_stored[n_cs,t] == mdl.cs_cold_stored[n_cs,t-1]*(1-ice.selfRelease)\
+                                             + step * (ice.efficiency*mdl.cs_cold_in[n_cs,t-1] - (1/ice.efficiency)*mdl.cs_cold_out[n_cs,t-1])
+optimalDispatch.coldStorageEnergyBalance = Constraint(N_cs,T,rule=coldStorageEnergyBalance)
+def coldStorageRampLimit(mdl,n_cs,t):
+    if t == 0:
+        return Constraint.Skip
+    else:
+        return -microgrid_device[n_cs].maxDetP <= (mdl.cs_cold_out[n_cs,t] - mdl.cs_cold_in[n_cs,t]) - (mdl.cs_cold_out[n_cs,t-1] - mdl.cs_cold_in[n_cs,t-1]) <= microgrid_device[n_cs].maxDetP
+optimalDispatch.coldStorageRampLimit = Constraint(N_cs,T,rule=coldStorageRampLimit)
+
+'''Define Objectives'''
+def OM_Cost(mdl):
+    om_cost = 0
+    for id in N_dev:
+        if(id in N_es):
+            om_cost += microgrid_device[id].om * step * (sum(mdl.es_power_out[id,t] for t in T)+sum(mdl.es_power_in[id,t] for t in T))
+        if(id in N_cs):
+            om_cost += microgrid_device[id].om * step * (sum(mdl.cs_cold_out[id,t] for t in T)+sum(mdl.cs_cold_in[id,t] for t in T))
+        if(id in N_absc):
+            om_cost += microgrid_device[id].om * step * sum(mdl.absc_heat_in[id,t] for t in T)
+        if(id in N_bol):
+            om_cost += microgrid_device[id].om * step * sum(mdl.bol_power[id,t] for t in T)
+        if(id in N_ac):
+            om_cost += microgrid_device[id].om * step * sum(mdl.ac_power[id,t] for t in T)
+    return om_cost
+def Dep_Cost(mdl):
+    return sum(sum(microgrid_device[id].Cbw * step * mdl.es_power_out[id,t] for id in N_es) for t in T)
+def Fuel_Cost(mdl):
+    fuel_cost = 0
+    for id in N_gt:
+        fuel_cost += (1/microgrid_device[id].efficiency) * microgrid_device['ut'].gas_price * step * sum(mdl.gt_power[id,t] for t in T)
+    for id in N_bol:
+        fuel_cost += (1/microgrid_device[id].efficiency) * microgrid_device['ut'].gas_price * step * sum(mdl.bol_power[id,t] for t in T)
+    return  fuel_cost
+def ElectricalFee(mdl):
+    return step * sum(mdl.utility_power[t]*microgrid_device['ut'].buy_price[t] for t in T)
+def HeatFee(mdl):
+    return  step*sum(mdl.high_heat[t] for t in T)*microgrid_device['ut'].steam_price
+def obj_Economical(mdl):
+    return OM_Cost(mdl) + Dep_Cost(mdl) + Fuel_Cost(mdl) + ElectricalFee(mdl) + HeatFee(mdl)
+optimalDispatch.obj_Economical = Objective(rule=obj_Economical)
+'''Solve the Model'''
+solver = SolverFactory('glpk')
+solver.solve(optimalDispatch)
+'''Retrieve the result'''
+ts=pd.date_range('2017/8/28 00:00:00',periods=96,freq='15min')
+df = pd.DataFrame()
+df['电负荷'] = pd.Series(electricalLoad)
+df['电价'] = pd.Series(microgrid_device['ut'].buy_price)
+df['电网购电功率'] = pd.Series([value(optimalDispatch.utility_power[t]) for t in T])
+for es in N_es:
+    df[es+'电池电量'] = pd.Series([value(optimalDispatch.es_energy[es, t]) for t in T])
+    df[es+'电储能充电功率'] = pd.Series([value(optimalDispatch.es_power_in[es,t]) for t in T])
+    df[es+'电储能放电功率'] = pd.Series([value(optimalDispatch.es_power_out[es, t]) for t in T])
+for gt in N_gt:
+    df[gt+'机组出力'] = pd.Series([value(optimalDispatch.gt_power[gt, t]) for t in T])
+    df[gt+'余热锅炉热功率'] = pd.Series([value(optimalDispatch.gt_power[gt, t])*microgrid_device[gt].HER*microgrid_device[gt].heat_recycle for t in T])
+df['光伏出力'] = pd.Series(pv_output)
+for ac in N_ac:
+    df[ac+'空调制冷耗电功率'] = pd.Series([value(optimalDispatch.ac_power[ac, t]) for t in T])
+    df[ac+'空调制冷功率'] = df[ac+'空调制冷耗电功率']*microgrid_device[ac].EER
+for cs in N_cs:
+    df[cs+'冰蓄冷耗电功率'] = pd.Series([value(optimalDispatch.cs_power[cs, t]) for t in T])
+    df[cs+'冰蓄冷储冷功率'] = pd.Series([value(optimalDispatch.cs_cold_in[cs, t]) for t in T])
+    df[cs+'冰蓄冷供冷功率'] = pd.Series([value(optimalDispatch.cs_cold_out[cs, t]) for t in T])
+    df[cs+'冰蓄冷制冷机直接供冷耗电功率'] = df[cs+'冰蓄冷耗电功率']*microgrid_device[cs].EER - df[cs+'冰蓄冷储冷功率']
+    df[cs+'冰蓄冷储冷量'] = pd.Series([value(optimalDispatch.cs_cold_stored[cs, t]) for t in T])
+for absc in N_absc:
+    df[absc+'吸收式制冷机制冷功率'] = pd.Series([value(optimalDispatch.absc_heat_in[absc, t]) for t in T]) * microgrid_device[absc].COP_htc
+for bol in N_bol:
+    df[bol+'燃气锅炉热功率'] = pd.Series([value(optimalDispatch.bol_power[bol, t]) for t in T])
+df.to_excel('output.xlsx')
+plt.plot(df)
+plt.show()
---
+++ microgrid.py
@@ -0,0 +1,175 @@
+class PV:
+    def __init__(self, om = 0.0005 , output = list()):
+        self.om = om
+        self.output = output
+    def show(self):
+        return {"om":self.om}
+
+
+class electricStorage:
+    def __init__(self, om = 0.005, Cbw = 0.00075 , capacity = 9600, SOCmin = 0.1, SOCmax = 0.9, SOCint = 0.1, Pmax_in = 1250, Pmax_out = 1250, efficiency = 0.9, selfRelease = 0.000):
+        self.om = om
+        self.Cbw = Cbw
+        self.capacity = capacity
+        self.SOCmin = SOCmin
+        self.SOCmax = SOCmax
+        self.SOCint = SOCint
+        self.Pmax_in = Pmax_in
+        self.Pmax_out = Pmax_out
+        self.efficiency = efficiency
+        self.selfRelease = selfRelease
+        self.maxDetP = self.Pmax_out * 0.5
+        self.power_into = {}
+        self.power_outof = {}
+        self.energy = {}
+    def show(self):
+        return {"om":self.om,
+                "Cbw":self.Cbw,
+                "capacity":self.capacity,
+                "SOCmin":self.SOCmin,
+                "SOCmax":self.SOCmax,
+                "SOCint":self.SOCint,
+                "Pmax_out":self.Pmax_out,
+                "Pmax_in":self.Pmax_in,
+                "selfRelease":self.selfRelease,
+                "efficiency":self.efficiency}
+
+
+class absorptionChiller:
+    def __init__(self, om = 0.00008, COP_htc = 1.2, COP_hth = 1, Hmin = 0, Hmax = 1000):
+        self.om = om
+        self.COP_htc = COP_htc
+        self.COP_hth = COP_hth
+        self.Hmin = Hmin
+        self.Hmax = Hmax
+        self.result = {}
+    def show(self):
+        return {"om":self.om,
+                "COP_htc":self.COP_htc,
+                "COP_hth":self.COP_hth}
+
+class boiler:
+    def __init__(self, om = 0.03, Pmax = 1000, Pmin = 0, efficiency = 0.9):
+        self.om = om
+        self.Pmin = Pmin
+        self.Pmax = Pmax
+        self.efficiency = efficiency
+        self.result = {}
+    def show(self):
+        return {"om":self.om,
+                "Pmax":self.Pmax,
+                "Pmin":self.Pmin,
+                "efficiency":self.efficiency}
+
+class heatStorage:
+    def __init__(self, om = 0.04, capacity = 2000, Tmin = 0, Tmax = 0.95, Tint = 0.1, Hmax_in = 1500, Hmax_out = 1500, selfRelease = 0.003):
+        self.om = om
+        self.capacity = capacity
+        self.Tmin = Tmin
+        self.Tmax = Tmax
+        self.Tint = Tint
+        self.Hmax_in = Hmax_in
+        self.Hmax_out = Hmax_out
+        self.selfRelease = selfRelease
+        self.maxDetP = self.Hmax_in * 0.4
+        self.result = {}
+    def show(self):
+        return {"om":self.om,
+                "capacity":self.capacity,
+                "Tmax":self.Tmax,
+                "Tint":self.Tint,
+                "Hmax_out":self.Hmax_out,
+                "Hmax_in":self.Hmax_in}
+
+class coldStorage:
+    def __init__(self, om = 0.01, capacity = 3000, Tmin = 0.1, Tmax = 0.95, Tint = 0.1, Hin = 500, Hout = 500, Pmin = 0, Pmax = 500, maxDetP = 500, EER = 3 , efficiency = 0.9 , COP = 3 , Partition_in = 0.9, Partition_out = 0.9, mode = "并联" ,selfRelease = 0.002):
+        self.om = om
+        self.capacity = capacity
+        self.Tmin = Tmin
+        self.Tmax = Tmax
+        self.Tint = Tint
+        self.Hin = Hin
+        self.Hout = Hout
+        self.Pmax = Pmax
+        self.Pmin = Pmin
+        self.EER = EER
+        self.efficiency = efficiency
+        self.COP = COP
+        self.Partition_in = Partition_in
+        self.Partition_out = Partition_out
+        self.mode = mode
+        self.maxDetP = maxDetP
+        self.selfRelease = selfRelease
+        self.result_electricity_tank = {}
+        self.result_cold_tank = {}
+        self.result_electricity_ref = {}
+    def show(self):
+        return {"om":self.om,
+                "capacity":self.capacity,
+                "Tmax":self.Tmax,
+                "Tmin":self.Tmin,
+                "Tint":self.Tint,
+                "Hin":self.Hin,
+                "Hout":self.Hout,
+                "COP":self.COP,
+                "Partition_out":self.Partition_out,
+                "Partition_in":self.Partition_in,
+                "EER":self.EER,
+                "efficiency":self.efficiency}
+
+class airConditioner:## P>=0 cooling, P<0 heating
+    def __init__(self, om = 0.0097, Pmax = 500, Pmin = 0, EER = 4.3 , COP = 3.6):
+        self.om = om
+        self.Pmax = Pmax
+        self.EER = EER
+        self.COP = COP
+        self.Pmin = Pmin
+        self.result = {}
+    def show(self):
+        return {"om":self.om,
+                "Pmax":self.Pmax,
+                "EER":self.EER,
+                "COP":self.COP}
+
+class gasTurbine:
+    def __init__(self, om = 0.063, Pmax = 1000, Pmin = 50, efficiency = 0.33, heat_recycle = 0.6):
+        self.om = om
+        self.Pmax = Pmax
+        self.Pmin = Pmin
+        self.efficiency = efficiency
+        self.heat_recycle = heat_recycle
+        self.HER = (1 - efficiency)/efficiency
+        self.result = {}
+    def show(self):
+        return {"om":self.om,
+                "Pmax":self.Pmax,
+                "Pmin":self.Pmin,
+                "efficiency":self.efficiency,
+                "HER":self.HER,
+                "heat_recycle":self.heat_recycle}
+
+
+class utility:
+    def __init__(self, buy_price = 0.8, sell_price = 0, gas_price = 0.349,steam_price = 348/996, CO2_utility = 0.997, CO2_user = 0.18):
+        self.buy_price = buy_price
+        self.sell_price = sell_price
+        self.gas_price = gas_price
+        self.result = {}
+        self.steam_price = steam_price
+        self.gas_utility = {}
+        self.CO2_utility = CO2_utility
+        self.CO2_user = CO2_user
+    def show(self):
+        return {"sell_price":self.sell_price,
+                "gas_price":self.gas_price}
+
+class inverter:
+    def __init__(self, ac_dc = 0.95 , dc_ac = 0.95,maxP = 5000):
+        self.ac_dc_efficiency = ac_dc
+        self.dc_ac_efficiency = dc_ac
+        self.maxP = maxP
+        self.result = {}
+    def show(self):
+        return {"ac_dc_efficiency":self.ac_dc_efficiency,
+                "dc_ac_efficiency":self.dc_ac_efficiency}
+
