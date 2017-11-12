9b507b04f27c33a37004a58b316871bf15262136
Master Shi <zidane765@hotmail.com>
2017-11-12 14:20:41 +0800

    增加了pb2.py和pb2_grpc.py文件，定义了service和message

--- dayahead.py
+++ dayahead.py
@@ -11,11 +11,11 @@ microgrid_data = pd.read_excel('input.xlsx')
 optimalDispatch = optimizationModel.DayAheadModel(microgrid_data,case)
 
 '''Solve the base model'''
-
 xfrm = TransformationFactory('gdp.chull')
 xfrm.apply_to(optimalDispatch)
 solver = SolverFactory('glpk')
 solver.solve(optimalDispatch)
+print('自趋优：'+str(value(optimalDispatch.objective)))
 '''Retrieve the result'''
 result = optimizationModel.retriveResult(microgrid_data,case,optimalDispatch)
 result.to_excel('output.xlsx')
@@ -25,9 +25,7 @@ optimizationModel.extendedResult(result)
 writer = pd.ExcelWriter('DayAhead Electric DR.xlsx')
 peak = range(72, 76)
 (max_model,max_amount) = optimizationModel.getMaxAmount(optimalDispatch,case,peak=peak,amount = [3000]*len(peak),mode='E')
-max_result = optimizationModel.retriveResult(microgrid_data,case,max_model)
-max_result.to_excel(writer,sheet_name = '1')
-for e in [0.8,0.6,0.4,0.2]:
+for e in [1,0.8,0.6,0.4,0.2]:
     amount = [e * max_amount[t] for t in range(max_amount.__len__())]
     #修正优化模型
     responseStrategy = optimizationModel.responseModel(optimalDispatch, case, peak=peak, amount=amount,mode='E')
@@ -36,6 +34,7 @@ for e in [0.8,0.6,0.4,0.2]:
     xfrm.apply_to(responseStrategy)
     solver = SolverFactory('glpk')
     solver.solve(responseStrategy)
+    print('电需求响应' + str(100*e) + '%：'+ str(value(responseStrategy.objective)))
     #获取结果
     res_result = optimizationModel.retriveResult(microgrid_data,case,responseStrategy)
     res_result.to_excel(writer,sheet_name = str(e))
@@ -44,12 +43,11 @@ writer.save()
 writer = pd.ExcelWriter('DayAhead Heat DR.xlsx')
 peak = range(72, 76)
 (max_model,max_amount) = optimizationModel.getMaxAmount(optimalDispatch,case,peak=peak,amount = [3000]*len(peak),mode='H')
-max_result = optimizationModel.retriveResult(microgrid_data,case,max_model)
-max_result.to_excel(writer,sheet_name = '1')
-for e in [0.8,0.6,0.4,0.2]:
+for e in [1,0.8,0.6,0.4,0.2]:
     amount = [e * max_amount[t] for t in range(max_amount.__len__())]
     # 修正优化模型并求解
     responseStrategy = optimizationModel.responseModel(optimalDispatch, case, peak=peak, amount=amount,mode='H')
+    print('热需求响应' + str(100 * e) + '%：' + str(value(responseStrategy.objective)))
     # 获取结果
     res_result = optimizationModel.retriveResult(microgrid_data, case, responseStrategy)
     res_result.to_excel(writer, sheet_name=str(e))
--- dayin.py
+++ dayin.py
@@ -16,6 +16,7 @@ xfrm = TransformationFactory('gdp.chull')
 xfrm.apply_to(optimalDispatch)
 solver = SolverFactory('glpk')
 solver.solve(optimalDispatch)
+print('自趋优：'+str(value(optimalDispatch.objective)))
 '''Retrieve the result'''
 result = optimizationModel.retriveResult(microgrid_data,case,optimalDispatch)
 result.to_excel('dayin.xlsx')
@@ -25,9 +26,7 @@ optimizationModel.extendedResult(result)
 writer = pd.ExcelWriter('DayIn Electric DR.xlsx')
 peak = range(82, 86)
 (max_model,max_amount) = optimizationModel.getMaxAmount(optimalDispatch,case,peak=peak,amount = [3000]*len(peak),mode='E')
-max_result = optimizationModel.retriveResult(microgrid_data,case,max_model)
-max_result.to_excel(writer,sheet_name = '1')
-for e in [0.8,0.6,0.4,0.2]:
+for e in [1,0.8,0.6,0.4,0.2]:
     amount = [e * max_amount[t] for t in range(max_amount.__len__())]
     #修正优化模型
     responseStrategy = optimizationModel.responseModel(optimalDispatch, case, peak=peak, amount = amount,mode='E')#TODO
@@ -36,6 +35,7 @@ for e in [0.8,0.6,0.4,0.2]:
     xfrm.apply_to(responseStrategy)
     solver = SolverFactory('glpk')
     solver.solve(responseStrategy)
+    print('电需求响应' + str(100 * e) + '%：' + str(value(responseStrategy.objective)))
     #获取结果
     res_result = optimizationModel.retriveResult(microgrid_data,case,responseStrategy)
     res_result.to_excel(writer,sheet_name = str(e))
@@ -44,12 +44,11 @@ writer.save()
 writer = pd.ExcelWriter('DayIn Heat DR.xlsx')
 peak = range(82, 86)
 (max_model,max_amount) = optimizationModel.getMaxAmount(optimalDispatch,case,peak=peak,amount = [3000]*len(peak),mode='H')
-max_result = optimizationModel.retriveResult(microgrid_data,case,max_model)
-max_result.to_excel(writer,sheet_name = '1')
-for e in [0.8,0.6,0.4,0.2]:
+for e in [1,0.8,0.6,0.4,0.2]:
     amount = [e * max_amount[t] for t in range(max_amount.__len__())]
     # 修正优化模型并求解
     responseStrategy = optimizationModel.responseModel(optimalDispatch, case, peak=peak, amount=amount,mode='H')
+    print('热需求响应' + str(100 * e) + '%：' + str(value(responseStrategy.objective)))
     # 获取结果
     res_result = optimizationModel.retriveResult(microgrid_data, case, responseStrategy)
     res_result.to_excel(writer, sheet_name=str(e))
