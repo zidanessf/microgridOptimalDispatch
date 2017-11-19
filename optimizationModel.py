e1f54c314df9e0bd7d6684a018322385cc1838e2
xuhang <xuhang_1994@163.com>
2017-11-19 11:28:58 +0800

    热品位约束的一些修改，针对万力轮胎

--- optimizationModel.py
+++ optimizationModel.py
@@ -150,10 +150,10 @@ def DayAheadModel(microgrid_data,case):
     optimalDispatch.HPB1 = Constraint(T, rule=lambda mdl, t: mdl.ht[t] == mdl.high_heat[t] + sum(
         mdl.bol_power[n_bol, t] for n_bol in N_bol) + sum(mdl.gt_power[n_gt, t] * microgrid_device[n_gt].HER * microgrid_device[n_gt].heat_recycle for n_gt in N_gt))
     optimalDispatch.HPB2 = Constraint(T,rule = lambda mdl,t:mdl.ht[t] >= steam_heat_load[t])
-    optimalDispatch.HPB3 = Constraint(T, rule=lambda mdl, t: mdl.medium_heat[t] == sum(mdl.gt_power[n_gt, t] * microgrid_device[n_gt].HER * microgrid_device[n_gt].low_heat_recycle for n_gt in N_gt))
-    optimalDispatch.HPB4 = Constraint(T, rule=lambda mdl, t: mdl.medium_heat[t] + mdl.ht[t] >= steam_heat_load[t] )
+    #optimalDispatch.HPB3 = Constraint(T, rule=lambda mdl, t: mdl.medium_heat[t] == sum(mdl.gt_power[n_gt, t] * microgrid_device[n_gt].HER * microgrid_device[n_gt].low_heat_recycle for n_gt in N_gt))
+    #optimalDispatch.HPB4 = Constraint(T, rule=lambda mdl, t: mdl.medium_heat[t] + mdl.ht[t] >= steam_heat_load[t] )
     optimalDispatch.HPB5 = Constraint(T, rule=lambda mdl, t: mdl.low_heat[t] == (H2M) * steam_heat_load[t])
-    optimalDispatch.HPB6 = Constraint(T, rule=lambda mdl, t: mdl.low_heat[t] + mdl.ht[t] + mdl.medium_heat[t] >= steam_heat_load[t] + sum(mdl.absc_heat_in[n_absc, t] for n_absc in N_absc))
+    optimalDispatch.HPB6 = Constraint(T, rule=lambda mdl, t: mdl.low_heat[t] + mdl.ht[t] >= steam_heat_load[t] + sum(mdl.absc_heat_in[n_absc, t] for n_absc in N_absc))
 
     # TODO 完善高中低品味热模型
     '''冷功率平衡约束'''
@@ -353,7 +353,7 @@ def retriveResult(microgrid_data,case,model):
     for bol in N_bol:
         df[bol + '燃气锅炉热功率'] = pd.Series([value(model.bol_power[bol, t]) for t in T],index=T)
     df['中品位热功率'] = pd.Series([value(model.high_heat[t]) for t in T],index=T)
-    df['低品位热功率'] = pd.Series([value(model.low_heat[t]) for t in T],index=T) + pd.Series([value(model.medium_heat[t]) for t in T],index=T)
+    #df['低品位热功率'] = pd.Series([value(model.low_heat[t]) for t in T],index=T) + pd.Series([value(model.medium_heat[t]) for t in T],index=T)
     '''demond response'''
     try:
         if model.mode == 'E':
@@ -440,7 +440,7 @@ def extendedResult(result):
     sheet5['余热锅炉低品位热功率'] =df_sum(result,[col for col in result.columns if '余热锅炉低品位热功率' in col])
     sheet5['蒸汽回收低品位热'] = H2M * result['蒸汽负荷']
     sheet5['中品位热功率'] = result['中品位热功率']
-    sheet5['低品位热功率'] = result['低品位热功率']
+    #sheet5['低品位热功率'] = result['低品位热功率']
     sheet5['蒸汽驱动负荷'] = -result['蒸汽负荷']
     sheet5['热负荷'] = sheet5['蒸汽驱动负荷']
     sheet5['吸收式制冷机耗热功率'] = -df_sum(result, [col for col in result.columns if '吸收式制冷机制冷功率' in col]) / 0.8
@@ -452,8 +452,8 @@ def extendedResult(result):
     sheet5colors = ['#f4f441', '#42f486', '#f442ee','#41b8f4','#4194f4']
     #plt.stackplot(result.index.values.tolist(), sheet5['购热功率'], sheet5['余热锅炉中品位热功率'], sheet5['余热锅炉低品位热功率'],sheet5['蒸汽回收低品位热'],sheet5['吸收式制冷机耗热功率'],
                   #colors=sheet5colors)
-    plt.stackplot(result.index.values.tolist(), sheet5['购热功率'], color='#f4f441')
-    plt.stackplot(result.index.values.tolist(), sheet5['余热锅炉中品位热功率'], bottom=sheet5['购热功率'], color='#42f486')
+    plt.bar(result.index.values.tolist(), sheet5['购热功率'], color='#f4f441')
+    plt.bar(result.index.values.tolist(), sheet5['余热锅炉中品位热功率'], bottom=sheet5['购热功率'], color='#42f486')
     first_legend = plt.legend([load], ('热负荷',))
     ax = plt.gca().add_artist(first_legend)
     plt.legend([mpatches.Patch(color=c) for c in sheet5colors], ['购热功率', '余热锅炉中品位热功率', '吸收式制冷机制冷功率','蒸汽回收低品位热','吸收式制冷机耗热功率'])
@@ -471,9 +471,9 @@ def extendedResult(result):
     load, = plt.plot(sheet6['吸收式制冷机耗热功率'], linewidth=3.0, linestyle='--', label='吸收式制冷机耗热功率')
     sheet6colors = ['#f4f441']
     plt.bar(result.index.values.tolist(), sheet6['蒸汽回收低品位热'], color='#f4f441')
-    first_legend = plt.legend([load], ('吸收式制冷机耗热功率'))
+    first_legend = plt.legend([load], ('吸收式制冷机耗热功率',))
     ax = plt.gca().add_artist(first_legend)
-    plt.legend([mpatches.Patch(color=c) for c in sheet6colors], ['蒸汽回收低品位热'])
+    plt.legend([mpatches.Patch(color=c) for c in sheet6colors], ['蒸汽回收低品位热',])
     plt.xlabel('时间(h)')
     plt.ylabel('功率(kW)')
     plt.show()
