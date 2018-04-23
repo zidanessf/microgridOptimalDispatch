e0bf3a3022e22ecd5973cdbd2c77f71aaecee5ff
zidanessf <zidane765@gmail.com>
2018-04-23 15:56:18 +0800

    增加trilevel分支

--- microgridStructure.py
+++ microgridStructure.py
@@ -78,66 +78,93 @@ device_IES = {
     'inv' : inverter()
 }
 case_IES = MicrogridCase_Simple(device=device_IES, NumOfTime=96)
-graph_PS = nx.Graph()
-graph_PS.add_nodes_from([0,1,2,3,4])
-graph_PS.add_edges_from([(0,1),(0,4),(1,2),(2,3),(3,4),(0,3)])
-graph_PS.node[0].update({
-    'ID' : 'A',
-    'device' : {
-        'Park City' : gasTurbine(Pmax=170,Pmin=10,Cost=15),
-        'Alta' : PV()
-    }
-})
-graph_PS.node[1].update({
-    'ID' : 'B',
-    'device': {}
-    })
-graph_PS.node[2].update({
-    'ID' : 'C',
-    'device':{
-        'Solitude' : gasTurbine(Pmax=520,Pmin=10,Cost=30)
-    }
-})
-graph_PS.node[3].update({
-    'ID' : 'D',
-    'device':{
-        'Sundance' : gasTurbine(Pmax=200,Pmin=10,Cost=40)
-    }
-})
-graph_PS.node[4].update({
-    'ID' : 'E',
-    'device':{
-        'Brighton':gasTurbine(Pmax=600,Pmin=10,Cost=20)
-    }
-})
-graph_PS.edge[0][1].update({
-    'R' : 0.281,
-    'X' : 2.81,
-    'Limit' : 400
-})
-graph_PS.edge[0][3].update({
-    'R' : 0.304,
-    'X' : 3.04,
-    'Limit' : None
-})
-graph_PS.edge[0][4].update({
-    'R' : 0.064,
-    'X' : 0.64,
-    'Limit' : None
-})
-graph_PS.edge[1][2].update({
-    'R' : 0.108,
-    'X' : 1.08,
-    'Limit' : None
-})
-graph_PS.edge[2][3].update({
-    'R' : 0.297,
-    'X' : 2.97,
-    'Limit' : None
-})
-graph_PS.edge[3][4].update({
-    'R' : 0.297,
-    'X' : 2.97,
-    'Limit' : 240
-})
-case_PS = MicrogridCase_Graph(graph=graph_PS,NumOfTime=96)
+# graph_PS = nx.Graph()
+# graph_PS.add_nodes_from([0,1,2,3,4])
+# graph_PS.add_edges_from([(0,1),(0,4),(1,2),(2,3),(3,4),(0,3)])
+# graph_PS.node[0].update({
+#     'ID' : 'A',
+#     'device' : {
+#         'Park City' : gasTurbine(Pmax=170,Pmin=10,Cost=15),
+#         'Alta' : PV()
+#     }
+# })
+# graph_PS.node[1].update({
+#     'ID' : 'B',
+#     'device': {}
+#     })
+# graph_PS.node[2].update({
+#     'ID' : 'C',
+#     'device':{
+#         'Solitude' : gasTurbine(Pmax=520,Pmin=10,Cost=30)
+#     }
+# })
+# graph_PS.node[3].update({
+#     'ID' : 'D',
+#     'device':{
+#         'Sundance' : gasTurbine(Pmax=200,Pmin=10,Cost=40)
+#     }
+# })
+# graph_PS.node[4].update({
+#     'ID' : 'E',
+#     'device':{
+#         'Brighton':gasTurbine(Pmax=600,Pmin=10,Cost=20)
+#     }
+# })
+# graph_PS.edge[0][1].update({
+#     'R' : 0.281,
+#     'X' : 2.81,
+#     'Limit' : 400
+# })
+# graph_PS.edge[0][3].update({
+#     'R' : 0.304,
+#     'X' : 3.04,
+#     'Limit' : None
+# })
+# graph_PS.edge[0][4].update({
+#     'R' : 0.064,
+#     'X' : 0.64,
+#     'Limit' : None
+# })
+# graph_PS.edge[1][2].update({
+#     'R' : 0.108,
+#     'X' : 1.08,
+#     'Limit' : None
+# })
+# graph_PS.edge[2][3].update({
+#     'R' : 0.297,
+#     'X' : 2.97,
+#     'Limit' : None
+# })
+# graph_PS.edge[3][4].update({
+#     'R' : 0.297,
+#     'X' : 2.97,
+#     'Limit' : 240
+# })
+
+# 赶工啦，临时写的一个PYPOWER ---- GRAPH 转换器
+from case39 import case39
+import numpy as np
+def ppc2graph(ppc):
+    graph_PS = nx.Graph()
+    ppc_nodes = ppc['bus'][:,0].astype(np.int)
+    graph_PS.add_nodes_from(ppc_nodes)
+    for bus in graph_PS.nodes():
+        graph_PS.node[bus].update({
+            'device':{}
+        })
+    gen = ppc['gen']
+    gencost = ppc['gencost']
+    for row in gen:
+        bus = row[0].astype(np.int)
+        graph_PS.node[bus]['device'].update({'GT_'+str(bus):gasTurbine(Pmax=row[8],Pmin=row[9],
+                                                                       Cost=0.3)})
+    for row in ppc['branch']:
+        nf = row[0].astype(np.int)
+        nt = row[1].astype(np.int)
+        graph_PS.add_edge(nf,nt,{'R':row[2],'X':row[3],'Limit':row[5]})
+
+    return graph_PS
+
+ppc = case39()
+graph_case39 = ppc2graph(ppc)
+case_PS = MicrogridCase_Graph(graph=graph_case39,NumOfTime=96)
