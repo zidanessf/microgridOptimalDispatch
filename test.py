e7fdd85893ad88d7161e4a914e82887075e43dc9
Master Shi <zidane765@hotmail.com>
2018-05-09 17:32:45 +0800

    增加了SCUC服务第一版，该版本使用了GUROBI求解器，算法本身是通用的，可以接受通用的网架、机组输入。不过目前输入输出的接口还没有做，测试算例用的是PJM-5节点。

--- test.py
+++ test.py
@@ -13,15 +13,10 @@ model.o = Objective(expr=2*model.x + 2*model.sub.y + 2*model.v - model.t)
 model.c = Constraint(expr=model.x + model.v >= 0)
 model.sub.o = Objective(expr=model.x + model.sub.w, sense=maximize)
 model.sub.c = Constraint(expr=2.29 <= model.sub.y + model.sub.w <= 2.3)
-
-def balance(mdl):
-    return mdl.y <= mdl.w
-model.sub.balance = Constraint(rule=balance)
 xfrm = TransformationFactory('bilevel.linear_dual')
 xfrm.apply_to(model)
 solver = SolverFactory('bilevel_blp_global')
 result = solver.solve(model)
 print(value(model.sub.y))
-+ mdl.gt_power[n_gt,t]*mdl.gt_power[n_gt,t]/microgrid_device[n_gt].Pmax
 
 #总结：pyomo可以处理上层带有整数变量或互补条件，下层为线性模型的双层规划模型。通过bilevel.linear_mpec进行转化，并通过bilevel_blp_global求解器求解
